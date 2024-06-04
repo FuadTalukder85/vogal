1bc0d7739f7ef0b2f49f7c3411631ce6608be9c6
FuadTalukder85 <webfuadtk@gmail.com>
2024-06-05 01:43:19 +0600

    added News & Articles

--- src/app/homePage/HomePage.jsx
+++ src/app/homePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../components/banner/Banner";
 import TopCategory from "../../components/topCategory/TopCategory";
+import Articles from "../../components/articles/Articles";
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
       <TopCategory></TopCategory>
+      <Articles></Articles>
     </div>
   );
 };
---
+++ src/components/articles/Articles.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import articleImg1 from "../../assets/images/articleImg/articleImg1.png";
+import articleImg2 from "../../assets/images/articleImg/articleImg2.png";
+import articleImg3 from "../../assets/images/articleImg/articleImg3.png";
+
+const Articles = () => {
+  return (
+    <div className="bg-[#F7F7F7]">
+      <div className="max-w-[1300px] mx-auto">
+        <h5 className="text-3xl font-semibold text-center mt-14">
+          News & Articles
+        </h5>
+        <div className="flex gap-5 mt-7">
+          <div className="bg-white">
+            <Image src={articleImg1} alt="articleImg1"></Image>
+            <h5 className="text-lg font-semibold text-center mt-4 pb-5">
+              Match outfits with other significant
+            </h5>
+          </div>
+          <div className="bg-white">
+            <Image src={articleImg2} alt="articleImg1"></Image>
+            <h5 className="text-lg font-semibold text-center mt-4 pb-5">
+              Match outfits with other significant
+            </h5>
+          </div>
+          <div className="bg-white">
+            <Image src={articleImg3} alt="articleImg1"></Image>
+            <h5 className="text-lg font-semibold text-center mt-4 pb-5">
+              Match outfits with other significant
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Articles;
